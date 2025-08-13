75c7b3e4146daf3856e6ff1f1f468b2b404cf41f
youdy <youdy017@gmail.com>
2025-08-13 22:21:33 +0700

    DecisionPaper page

--- app/Http/Controllers/PaperController.php
+++ app/Http/Controllers/PaperController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Inertia\Response;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -81,5 +82,57 @@ class PaperController extends Controller
         $paper->delete();
         
         return redirect()->route('papers.index');
+    }
+    /**
+     * Display the decisions for papers
+     */
+    
+    public function decisions(): Response
+    {
+        // For now, we will use sample data for the index page.
+        $paper = (object)[
+            'id' => 1, // Sample ID
+            'title' => 'The Future of AI in Academic Research',
+            'author' => 'Dr. Alex Ray',
+            'track' => 'Artificial Intelligence',
+        ];
+
+        $reviews = [
+            ['id' => 1, 'reviewer' => 'Panha', 'status' => 'Accept'],
+            ['id' => 2, 'reviewer' => 'Ronaldo', 'status' => 'Reject'],
+            ['id' => 3, 'reviewer' => 'Ngorl Ngorl', 'status' => 'Revise'],
+        ];
+
+        // Now, we pass the data to the correct component.
+        return Inertia::render('PaperDecision/Index', [
+            'paper' => $paper,
+            'reviews' => $reviews,
+        ]);
+    }
+    public function decisionShow($id): Response
+    {
+        // In a real app, you would find the specific paper by its $id
+        // $paper = Paper::findOrFail($id);
+        // $reviews = $paper->reviews;
+
+        $paper = (object)[
+            'id' => $id,
+            'title' => 'The Future of AI in Academic Research',
+            'author' => 'Dr. Alex Ray',
+            'track' => 'Artificial Intelligence',
+        ];
+
+        $reviews = [
+            ['id' => 1, 'reviewer' => 'Panha', 'status' => 'Accept'],
+            ['id' => 2, 'reviewer' => 'Ronaldo', 'status' => 'Reject'],
+            ['id' => 3, 'reviewer' => 'Ngorl Ngorl', 'status' => 'Revise'],
+        ];
+
+        // This renders the component for the 'show' page.
+        // It's good to use a different component for this later.
+        return Inertia::render('PaperDecision/ReviewedPaper', [
+            'paper' => $paper,
+            'reviews' => $reviews,
+        ]);
     }
 }
